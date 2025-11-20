0593aeebdf91a3a2c0c9aa33b4b49fc0e9fea5f9
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2025-11-20 10:32:54 +0100

    Update .ts

--- Commande-Restaurant/OrderLine.ts
+++ Commande-Restaurant/OrderLine.ts
@@ -1,13 +1,14 @@
 import { Dish } from "./dish";
 
 export class OrderLine {
-    constructor(
-    public dish : Dish,
+  constructor(
+    public dish: Dish,
     public quantity: number
-) {}
+  ) {}
 
-getLineTotal(): number {
-    return this.dish.price * this.quantity;
-}
-
+  getLineTotal(): number {
+    const total = this.dish.price * this.quantity;
+    console.log(`     (Calcul ligne) ${this.quantity} x ${this.dish.name} = ${total} €`);
+    return total;
+  }
 }
