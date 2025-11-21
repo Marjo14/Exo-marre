c6c98a00a4d068c13ac0312b760b8909fedb8a2b
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2025-11-21 11:30:20 +0100

    recupere api response

--- EXO-TS/exerciceTs.ts
+++ EXO-TS/exerciceTs.ts
@@ -1,3 +1,4 @@
+1// Define datas type
 interface Todo {
   id: number;
   todo: string;
@@ -11,3 +12,27 @@ interface TodosResponse {
   skip: number;
   limit: number;
 }
+
+2// Récupérer les information de l'API 
+async function fetchTodos(): Promise<TodosResponse> {
+  const response = await fetch("https://dummyjson.com/todos");
+  console.log("RAW RESPONSE :", response);
+
+  if (!response.ok) {
+    throw new Error(`Error HTTP : ${response.status}`);
+  }
+
+  const data: TodosResponse = await response.json();
+  console.log("DATA JSON :", data); 
+
+  return data;
+}
+
+async function main() {
+  const result = await fetchTodos();
+  console.log("RESULT in Main :", result);
+}
+
+main();
+
+
