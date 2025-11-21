10fd6067a914f6a2ab08d039aecffbb044e04fbe
Marjolaine Baudin <baudin.marjolaine@gmail.com>
2025-11-21 14:38:27 +0100

    Create file generic to implement generic method

---
+++ EXO-TS/generic.ts
@@ -0,0 +1,35 @@
+// 1) Interfaces 
+interface Todo {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+}
+
+interface TodosResponse {
+  todos: Todo[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+// 2) Fonction générique
+async function getDummy<T>(url: string): Promise<T> {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Erreur HTTP : ${response.status}`);
+  }
+
+  const data: T = await response.json(); 
+  return data;
+}
+
+// 3) Fonction main qui utilise getDummy
+async function main() {
+  const todosResponse = await getDummy<TodosResponse>("https://dummyjson.com/todos");
+  console.log(todosResponse);
+}
+
+// 4) Appel de main (en dehors de main !)
+main();
